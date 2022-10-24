41fe81966b71116be612c671309490ade5d4faf6
seoheui <115434894+seoheui@users.noreply.github.com>
2022-10-24 21:36:15 +0900

    Add files via upload

---
+++ Day01.py
@@ -0,0 +1,14 @@
+print('#'*40)
+# print = 0 : 오류가 생겨버림. 프린트 함수가 0이 된다. 자유도가 너무 높아....
+print("My first Program")
+
+my_name = 'seohee\'s steakhouse' # =은 단순히 메모리 공간에 할당하는 것
+                    # C, Java : 한글자 '', 여러글자 ""
+                    # 역슬래쉬를 주면 문자로 인식
+
+your_name = "noname"
+his_name =  '"sleepy" joe'
+
+print(my_name)
+print(your_name)
+print(his_name)
